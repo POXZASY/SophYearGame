235b1e760ca84859f23bb8487dca12916c1a6399
Unknown <thorowi1@binghamton.edu>
2018-09-10 16:14:22 -0400

    more finished province

--- src/game/Province.java
+++ src/game/Province.java
@@ -3,8 +3,48 @@ package game;
 import java.util.*;
 
 public class Province {
-	public Province(String name, Province[] bordering, int army, Player owner){
-		
+	private String name;
+	private ArrayList<Province> borders = new ArrayList<>();
+	private int troopNums;
+	private Player onwer;
+	
+	public Province(String nme, ArrayList<Province> bordering, int army, Player owning){
+		name = nme;
+		borders = bordering;
+		troopNums = army;
+		onwer = owning;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public ArrayList<Province> getBorders() {
+		return borders;
+	}
+
+	public void setBorders(ArrayList<Province> borders) {
+		this.borders = borders;
+	}
+
+	public int getTroopNums() {
+		return troopNums;
+	}
+
+	public void setTroopNums(int troopNums) {
+		this.troopNums = troopNums;
+	}
+
+	public Player getOnwer() {
+		return onwer;
+	}
+
+	public void setOnwer(Player onwer) {
+		this.onwer = onwer;
 	}
 
 }
