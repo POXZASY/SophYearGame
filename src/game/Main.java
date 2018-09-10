0a18ad81ac5f67fbf4ed4517e806afa99aa3166c
Unknown <thorowi1@binghamton.edu>
2018-09-10 16:54:56 -0400

    Some provinces created

--- src/game/Main.java
+++ src/game/Main.java
@@ -1,9 +1,33 @@
 package game;
 
+import java.util.Arrays;
+import java.util.ArrayList;
+
 public class Main {
 	
 	public static void main(String args[]) {
-		
+		Province north_africa = new Province("North Africa");
+		Province egypt = new Province("Egypt");
+		Province east_africa = new Province("East Africa");
+		Province congo = new Province("Congo");
+		Province south_africa = new Province("South_Africa");
+		Province madagascar = new Province("Madagascar");
+		Province middle_east = new Province("Middle East");
+		Province afghanistan = new Province("Afghanistan");
+		Province ural = new Province("Ural");
+		Province siberia = new Province("Siberia");
+		Province yakutsk = new Province("Yakutsk");
+		Province Kamchatka = new Province("Kamchatka");
+		Province irkutsk = new Province("Irkutsk");
+		Province mongolia = new Province("Mongola");
+		Province japan = new Province("Japan");
+		Province china = new Province("China");
+		Province india = new Province("India");
+		Province siam = new Province("Siam");
+		Province indonesia = new Province("Indonesia");
+		Province new_guinea = new Province("New Guinea");
+		Province western_australia = new Province("Western Australia");
+		Province eastern_australia = new Province("Eastern Australia");
 	}
 
 }
